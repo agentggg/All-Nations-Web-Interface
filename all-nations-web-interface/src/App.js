30d541edb8d19f497867829071a8dbd6d7ecb8c3
Tech & Faith <47467793+agentggg@users.noreply.github.com>
2025-11-04 10:33:07 -0500

    creating gh pages

--- all-nations-web-interface/src/App.js
+++ all-nations-web-interface/src/App.js
@@ -1,27 +1,29 @@
+// App.js
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SendAll from "./components/send_all";
 import Login from "./components/login";
 import SendByCategory from "./components/send_by_category";
-// import SendAll from "./views/SendAll";
 import SendBySelectedContacts from "./components/send_by_selected_contatc";
 import SendByAmbassador from "./components/send_by_ambassador";
-// import SendByOutreach from "./views/SendByOutreach";
 import Home from "./components/home";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  // "/" in dev, "/All-Nations-Web-Interface" in prod (GH Pages)
+  const basename =
+    process.env.NODE_ENV === "production" ? "/All-Nations-Web-Interface" : "/";
+
   return (
-    <Router>
+    <Router basename={basename}>
       <Routes>
-        <Route path="login" element={<Login />} />
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/send_all" element={<SendAll />} />
-        <Route path="/send_by_category" element={<SendByCategory />}/>
+        <Route path="/send_by_category" element={<SendByCategory />} />
         <Route path="/send_by_ambassador" element={<SendByAmbassador />} />
-         <Route path="/send_by_selected_contact" element={<SendBySelectedContacts />} /> 
+        <Route path="/send_by_selected_contact" element={<SendBySelectedContacts />} />
       </Routes>
     </Router>
   );
 }
-
 export default App;
